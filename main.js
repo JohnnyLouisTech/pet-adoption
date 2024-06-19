9c696b8f9ddbe46f1fdd1da1c8a9eb886865c992
Johnny <jlouis26@gmail.com>
2024-06-19 14:58:32 -0400

    Working progress, fetched pet data

--- main.js
+++ main.js
@@ -8,3 +8,16 @@ async function start() {
 }
 
 start()
+
+async function petsArea() {
+  const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
+  const petsData = await petsPromise.json()
+  petsData.forEach(pet => {
+    console.log(pet.name)
+
+
+  })
+
+}
+
+petsArea()
